f96ace86696b08c41e3dab36e859d1c33f44339d
Aman Kumar Maurya <amankrmj@gmail.com>
2026-07-16 15:25:50 +0530

    fix(openapi): Remove spring-boot-starter-webflux to resolve ControllerAdviceBean conflict

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
 
 	// 3. Spring Cloud & Discovery
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
