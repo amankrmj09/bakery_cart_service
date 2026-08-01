e5ab471e78878a9074f047eaec8f8769478fc37c
Aman Kumar Maurya <amankrmj@gmail.com>
2026-08-01 10:23:53 +0530

    chore(config): update docker configs and tests for cart service

--- src/test/java/com/blubugtech/bakery_cart_service/BakeryCartServiceApplicationTests.java
+++ src/test/java/com/blubugtech/bakery_cart_service/BakeryCartServiceApplicationTests.java
@@ -1,9 +1,11 @@
 package com.blubugtech.bakery_cart_service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BakeryCartServiceApplicationTests {
 
     @Test
